f6b5cb4758b503f9382cee0a38731eb0d4b92d26
Oleksandr-Obodov <oleksandr.obodov@ardas.biz>
2023-04-15 21:18:28 +0300

    Added rest of methods to HW19

--- HW19/src/Main.java
+++ HW19/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -8,24 +9,31 @@ public class Main {
         Product product = new Product("Book");
         List<Product> products = new ArrayList<Product>() {
             {
-                add(new Product("Milk", 25, false));
-                add(new Product("Apple", 10, false));
-                add(new Product("Book", 251, true));
-                add(new Product("Book", 300, true));
-                add(new Product("Book", 150, false));
+                add(new Product("Milk", 25, false, new Date()));
+                add(new Product("Apple", 10, false, new Date()));
+                add(new Product("Book", 251, true, new Date()));
+                add(new Product("Book", 300, true, new Date()));
+                add(new Product("Book", 70, false, new Date()));
             }
         };
-        System.out.println("\nresult of searching Product with category 'Book' and price > 250");
+        System.out.println("result of searching Product with category 'Book' and price > 250");
         System.out.println(product.getProducts(products));
         System.out.println("\nresult of searching Product with category 'Book' and set discount 10%");
         System.out.println(product.getProductsWithDiscount(products));
 
 
         try {
-            System.out.println("\ngetLeastExpensiveBook");
+            System.out.println("\nresult of getLeastExpensiveBook():");
             System.out.println(product.getLeastExpensiveBook(products));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        System.out.println("\nresult of searching last three added products");
+        System.out.println(product.getLastThreeAddedProducts(products));
+        System.out.println("\nresult of getThisYearProductWithBookTypeAndFixedPrice():");
+        System.out.println(product.getThisYearProductWithBookTypeAndFixedPrice(products));
+        System.out.println("\nresult of sortProductsByType():");
+        System.out.println(product.sortProductsByType(products));
     }
 }
--- HW19/src/Product.java
+++ HW19/src/Product.java
@@ -1,24 +1,29 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Product {
 
+    private final String pattern = "dd-MM-yyyy";
     private String type;
     private double price;
-
     private boolean isDiscountApplied;
-
     private String category;
+    private Date date;
 
     public Product(String category) {
         this.category = category;
     }
 
-    public Product(String type, double price, boolean isDiscountApplied) {
+    public Product(String type, double price, boolean isDiscountApplied, Date date) {
         this.type = type;
         this.price = price;
         this.isDiscountApplied = isDiscountApplied;
+        this.date = date;
     }
 
     public String getType() {
@@ -41,8 +46,9 @@ public class Product {
         return isDiscountApplied;
     }
 
-    public void setDiscountApplied(boolean discountApplied) {
-        isDiscountApplied = discountApplied;
+
+    public Date getDate() {
+        return date;
     }
 
     public List<Product> getProducts(List<Product> productsList) {
@@ -65,9 +71,28 @@ public class Product {
 
     public Product getLeastExpensiveBook(List<Product> productsList) throws Exception {
         return productsList.stream()
-                   .filter(product -> product.getType().equals(category))
-                   .min(Comparator.comparingDouble(Product::getPrice))
-                   .orElseThrow(() -> new Exception("Product category: " + category + " not found"));
+                           .filter(product -> product.getType().equals(category))
+                           .min(Comparator.comparingDouble(Product::getPrice))
+                           .orElseThrow(() -> new Exception("Product category: " + category + " not found"));
+    }
+
+    public List<Product> getLastThreeAddedProducts(List<Product> productsList) {
+        return productsList.stream().skip(productsList.size() - 3).collect(Collectors.toList());
+    }
+
+    public List<Product> getThisYearProductWithBookTypeAndFixedPrice(List<Product> productList) {
+        return productList.stream()
+                          .filter(
+                              product -> (1900 + product.getDate().getYear()) == LocalDate.now().getYear()) //This is trash =) working with old Date()`s methods
+                          .filter(product -> product.getType().equals(category))
+                          .filter(product -> product.getPrice() < 75)
+                          .collect(Collectors.toList());
+    }
+
+    public Map<String, List<Product>> sortProductsByType(List<Product> productList) {
+        return productList.stream()
+                          .sorted(Comparator.comparing(Product::getType))
+                          .collect(Collectors.groupingBy(Product::getType));
     }
 
     @Override
@@ -76,6 +101,7 @@ public class Product {
                "type='" + type + '\'' +
                ", price=" + price +
                ", isDiscountApplied=" + isDiscountApplied +
+               ", createDate=" + new SimpleDateFormat(pattern).format(date) +
                '}';
     }
 }
